001bb2861cd66f871902688a364d7c38b441c53f
A Syed Arshad <syedarshad6601@gmail.com>
2026-01-03 12:58:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -390,4 +390,6 @@ Loan Department
         print(f"❌ Failed to send confirmation email to {receiver_email}: {e}")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+
+    app.run(host="0.0.0.0", port=8000, debug=True)
+    
